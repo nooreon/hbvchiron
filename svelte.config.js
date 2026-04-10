8acb034c14650b27a7823ca355c4cda687f9ed1c
Nooreon <25660871+nooreon@users.noreply.github.com>
2026-04-10 22:50:08 +0200

    paths fixen

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,11 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
     kit: {
         adapter: adapter({
-            pages: "build",
-            assets: "build",
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
+            fallback: "404.html"
+        }),
+        paths: {
+            base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
+        }
     }
 };
 
